f2aa719beb40ce7cdc5c8ba6c58ecbcb6448b756
Scott Alexander-Bown <dev@scottyab.com>
2016-01-05 15:38:10 +0000

    Updated sample and screenshot to include example of monospace attrib

--- app/src/main/java/com/scottyab/sample/MainActivity.java
+++ app/src/main/java/com/scottyab/sample/MainActivity.java
@@ -1,15 +1,22 @@
 package com.scottyab.sample;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.scottyab.sample.R;
 
 public class MainActivity extends AppCompatActivity {
+
+  private static String GITHUB_LINK = "https://github.com/scottyab/showhidepasswordedittext";
+
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +24,26 @@ public class MainActivity extends AppCompatActivity {
     setContentView(R.layout.activity_main);
     Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
+  }
 
 
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.menu_main, menu);
+    return true;
+  }
 
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    int id = item.getItemId();
+    if (id == R.id.action_github) {
+      Intent i = new Intent(Intent.ACTION_VIEW);
+      i.setData(Uri.parse(GITHUB_LINK));
+      startActivity(i);
+      return true;
+    }
+    return super.onOptionsItemSelected(item);
   }
 
 }
